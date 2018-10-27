2893e488feb48b6a2c15fcadf3b6b96da5b48f80
shehryarahmed <shehreyarahmed5@gmail.com>
2018-10-27 15:41:58 +0500

    add logic opreatar

--- Mongodb-demo/index.js
+++ Mongodb-demo/index.js
@@ -21,17 +21,43 @@ async function createCourses(){
         name:'Node.js Courses',
         author:'shehryarAhmed',
         tags: ['node', 'backend'] ,
-        isPublished:true
+        isPublished:false
     });
     const result = await course.save();
     console.log(result)
 
 }
 
+//Equal opreater
+//eq = (equal)
+//ne = (not equal)
+//gt = (greater than)
+//gte = (greater than or equal to)
+//lt = (less than)
+//lte = (less than or equal to)
+//in
+//nin = not in
+
+
+//logical Query
+// or
+// and
 
 async function getCourses(){
     const course = await Course.
-    find({author:"shehryarAhmed", isPublished: true})
+    // find({author:"shehryarAhmed", isPublished: true})
+    // find({price: 10})
+    //get more than 10 or equal to 10 
+    //$ dollar sign Represent opreator
+    //get courses 10 to 20 b/w
+        // find({price:{$gte:10,lte:20}})
+    //get courses only 10, 15 and 20 dollar price
+    // find({price : {$in:[10,15,20]}})
+
+
+    // find({author:"shehryarAhmed", isPublished: true})
+        find()
+        .and([{author:"shehryarAhmed"}, {isPublished: false}])
         .limit(10)
         .sort({name:1})
         .select({name:1});
