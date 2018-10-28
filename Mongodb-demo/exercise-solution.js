e88625473846c93dd45522e4b633e4cae0456d89
shehryarahmed <shehreyarahmed5@gmail.com>
2018-10-28 05:12:04 +0500

    error on updating Query

--- Mongodb-demo/exercise-solution.js
+++ Mongodb-demo/exercise-solution.js
@@ -19,7 +19,7 @@ async function getCourses(){
     return await Course
     // .find({isPublished:true, tags: {$in: ['backend','frontend']} })
     .find({isPublished:true})
-    .and([
+    .or([
         {price:{$gte:15}},
         {name: /.*by.*/i}
         ])
@@ -36,4 +36,30 @@ async function run(){
 }
 
 
-run();
+
+
+//Query First
+async function updateCourses(dd){
+    const course = await Course.findById(dd)
+    console.log(course,"gfsg")
+    if(!course) return;
+
+    course.isPublished = true;
+    course.author = 'Another Author';
+
+    // both are identical way to update data
+    // course.set({
+    //     isPublished: true,
+    //     author:'Another Author'
+    // })
+
+    return await course.save();
+    // console.log(result)
+ }
+
+ async function runUpdate(id){
+    const update = await updateCourses(id);
+    console.log(update)
+}
+runUpdate('5a68ff090c553064a218a547');
+// run();
